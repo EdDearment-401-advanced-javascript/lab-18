02b6d2493878995cb5f27c5c91504c8e9984ea3f
ekearm <ed787.ed@gmail.com>
2019-06-19 13:44:43 -0700

    added some basic scaffolding

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 'use strict';
 
 const fs = require('fs');
-
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3000');
 
 
 // const alterFile = (file) => {
---
+++ src/alterfile.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const fs = require('fs');
+const io = require('socket.io-client');
+const socket = io.connect('http://localhost:3000');
