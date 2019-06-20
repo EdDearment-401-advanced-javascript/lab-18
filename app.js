f145eaf1de643f0875c0d91685188507cd87bd51
ekearm <ed787.ed@gmail.com>
2019-06-19 20:02:14 -0700

    added mock

--- app.js
+++ app.js
@@ -4,6 +4,20 @@ const fs = require('fs');
 const io = require('socket.io-client');
 const socket = io.connect('http://localhost:3000');
 
+const readFS = require('./src/alterfile');
+const toUpp = require('./src/alterfile');
+const writFS = require('./src/alterfile');
+
+const alterFile = (file) => {
+  alterFile.readFS(file)
+    .then(data => {
+      data = alter.toUpp(data);
+      alter.writFS(file, data);
+    })
+    .then (() => socket.emit('file-save', file) && socket.disconnect(true), 500)
+    .catch(err => socket.emit('file-error'), 500);
+}
+
 
 // const alterFile = (file) => {
 //   fs.readFile( file, (err, data) => {
--- src/alterfile.js
+++ src/alterfile.js
@@ -3,3 +3,4 @@
 const fs = require('fs');
 const io = require('socket.io-client');
 const socket = io.connect('http://localhost:3000');
+
